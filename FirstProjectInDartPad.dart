b9a786c090ca3fa8f631d285577625d9b22bae78
Gustavo <80295452+hunteradv@users.noreply.github.com>
2023-02-27 20:19:56 -0300

    Finalizando primeiro projeto

--- FirstProjectInDartPad.dart
+++ FirstProjectInDartPad.dart
@@ -1,18 +1,38 @@
 enum Color { Red, Blue, White, Black }
 
-class Button {
+abstract class Widget {}
+
+class Icon extends Widget {
+  String icon;
+  double radius;
+
+  Icon(this.icon, {this.radius = 1});
+}
+
+class Text extends Widget {
   String text;
+  int size;
+  String fontFamily;
+
+  Text(this.text, {this.size = 12, this.fontFamily = "Arial"});
+}
+
+class Button {
+  Widget child;
   Color? color;
   double width;
   double height;
 
-  Button(this.text, {this.width = 0, this.height = 0, this.color});
+  Button(this.child, {this.width = 0, this.height = 0, this.color});
 }
 
 main() {
-  var btnSave = Button("oi", width: 2);
-  btnSave.color = Color.Red;
-  print(btnSave.text.toLowerCase());
-  print(btnSave.width);
-  print(btnSave.color);
+  //var icon = Icon("home");
+  //var text = Text("Salvar", size: 14);
+
+  //var btnSave = Button(icon);
+  //btnSave.color = Color.Red;
+
+  var btnSave = Button(Text("Salvar", fontFamily: "Robot"), color: Color.Red);
+  var btnHome = Button(Icon("Home", radius: 0.2), color: Color.Blue);
 }
